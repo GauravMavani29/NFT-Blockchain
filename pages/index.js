20b4865fca3901927d6b8a01043900378223b09e
GauravMavani29 <gauravmavani29@gmail.com>
2022-12-12 14:56:07 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
+}
+
+export default Home
